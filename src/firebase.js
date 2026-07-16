997f605568ac6aaa7bbc36a268bf07bb3d9d89f6
Aqeel Humood <aqeelhumood2@gmail.com>
2026-07-16 05:23:00 +0300

    Final update

--- src/firebase.js
+++ src/firebase.js
@@ -3,25 +3,14 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 import { getAuth } from 'firebase/auth'
 
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
-import { getAuth } from 'firebase/auth'
-
 const firebaseConfig = {
-  apiKey: "AIzaSyC07wUCcCPCFTcZkFV-3g9F-BDvfIAv_II",
-  authDomain: "blanco-menu.firebaseapp.com",
-  projectId: "blanco-menu",
-  storageBucket: "blanco-menu.firebasestorage.app",
-  messagingSenderId: "411043575202",
-  appId: "1:411043575202:web:27a8cb5255db0db3b6aefb"
+  apiKey: 'AIzaSyC07wUCcCPCFTcZkFV-3g9F-BDvfIAv_II',
+  authDomain: 'blanco-menu.firebaseapp.com',
+  projectId: 'blanco-menu',
+  storageBucket: 'blanco-menu.firebasestorage.app',
+  messagingSenderId: '411043575202',
+  appId: '1:411043575202:web:27a8cb5255db0db3b6aefb',
 }
-
-const app = initializeApp(firebaseConfig)
-
-export const db = getFirestore(app)
-export const storage = getStorage(app)
-export const auth = getAuth(app)
 
 const app = initializeApp(firebaseConfig)
 
